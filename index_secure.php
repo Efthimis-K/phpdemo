f395a20bddb4eb2223c586583bed84d2a8144798
Efthimios Karachondros <thimioskarah21@gmail.com>
2026-03-19 16:41:46 +0200

    Updated index_secure.php

--- index_secure.php
+++ index_secure.php
@@ -1,9 +1,10 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "fullform"; // Specify your database name
+// Database configuration, names omitted for security
+$servername = ""; // Server name
+$username = ""; // Username
+$password = ""; // Password
+$database = ""; // Specify your database name
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
